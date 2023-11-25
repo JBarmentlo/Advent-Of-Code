f8e4a0ece1fa95021164c0981dfaf265ab1cc3d5
Joep B <joepbarmentlo@gmail.com>
2023-11-25 13:08:21 +0100

    first day done

--- calories/src/main.rs
+++ calories/src/main.rs
@@ -1,18 +1,43 @@
 use std::fs;
 
+
 fn main() {
-    let contents = fs::read_to_string("data.txt").expect("File unreadable");
+    first_half();
+    second_half();
+}
+
+// TODO: look for elegant immutable solution
+fn second_half() {
+    let contents = fs::read_to_string("data.txt").expect("The file is static and is always parsable");
+
+    let mut sorted: Vec<u32> = contents.split("\n\n").map(|line_group| {
+        line_group.lines()
+        .map(|line| {
+            line.trim().parse::<u32>().expect("All the lines are parsable")
+        })
+        .sum()
+    })
+    .collect();
+    
+    sorted.sort();
+    let total: u32 = sorted.iter().rev().take(3).sum();
+
+    println!("top three: {total}");
+}
+
+
+fn first_half() {
+    let contents = fs::read_to_string("data.txt").expect("The file is static and is always parsable");
 
     let max: u64 = contents.split("\n\n").map(|line_group| {
         line_group.lines()
         .map(|line| {
-            line.trim().parse::<u64>().expect("Unparsable line: {line}")
+            line.trim().parse::<u64>().expect("All the lines are parsable")
         })
         .sum()
     })
     .max()
-    .expect("Should be a max");
-
-                                    
-    println!("{max}");
+    .expect("Fixed input file garantees that there is a max");
+            
+    println!("max: {max}");
 }
