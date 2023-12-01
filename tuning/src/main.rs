1cf38a83bc9f06c29fa8b8ac473c2038730c58ba
Joep B <joep.barmentlo@echopenfactory.com>
2023-12-01 13:14:01 +0100

    phase two complete

--- tuning/src/main.rs
+++ tuning/src/main.rs
@@ -3,12 +3,15 @@ use itertools::Itertools;
 
 fn main() {
     let text = fs::read_to_string("data.txt").expect("Fixed input");
-    let marker = get_marker(&text, 4);
+    let marker = get_marker(&text, 14);
     println!("marker: {marker}");
 }
 
 fn get_marker(text: &str, n_distinct: usize) -> usize {
-    let mut window = Vec::from(['0', '0', '0', '0']);
+    let mut window = Vec::new();
+    for _ in 0..n_distinct {
+        window.push('0');
+    }
 
     println!("{text}");
     text.chars().enumerate()
@@ -60,4 +63,46 @@ mod tests {
 
         assert_eq!(marker, 11);
     }
+
+
+    #[test]
+    fn second_one() {
+        let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        let marker = get_marker(input, 14);
+
+        assert_eq!(marker, 19);
+    }
+
+    #[test]
+    fn second_two() {
+        let input = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        let marker = get_marker(input, 14);
+
+        assert_eq!(marker, 23);
+    }
+
+    #[test]
+    fn second_three() {
+        let input = "nppdvjthqldpwncqszvftbrmjlhg";
+        let marker = get_marker(input, 14);
+
+        assert_eq!(marker, 23);
+    }
+
+    #[test]
+    fn second_four() {
+        let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        let marker = get_marker(input, 14);
+
+        assert_eq!(marker, 29);
+    }
+    #[test]
+    
+    fn second_five() {
+        let input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+        let marker = get_marker(input, 14);
+
+        assert_eq!(marker, 26);
+    }
+
 }
