c6f905f16429826f0f1ed5b7fce4c72b37fca736
Joep B <joepbarmentlo@gmail.com>
2024-02-24 17:08:51 +0100

    WIP: merge no comp errors

--- folders/src/main.rs
+++ folders/src/main.rs
@@ -36,46 +36,25 @@ impl Folder {
         }
     }
 
-    // fn merge(&mut self, other: Option<&Folder>) {
-    //     match other {
-    //         None => (),
-    //         Some(f) => {
-    //             f.subfolders.iter().map(|(name, folder)| self.subfolders.entry(name.clone()).and_modify(|my_folder| my_folder.merge(Some(folder))).or_insert(*folder));
-    //         }
-    //     }
-    // }
-
-    fn merge(&self, other: &Folder) -> Folder {
-        let merged_subfolders: HashMap<String, Folder> = self.subfolders.iter().map(|(name, folder)| {
-            match other.subfolders.get(name) {
-                None => (name.clone(), folder.clone()),
-                Some(other_folder) => (name.clone(), Self::merge(folder, other_folder))
-            }
-        })
-        .collect();
-
-        let merged_files: HashMap<String, File> = self.files.drain().chain(other.files.drain()).collect();
-        Folder { 
-            files: merged_files, 
-            subfolders: merged_subfolders 
+    fn merge(self, other: Folder) -> Folder {
+        Folder {
+            files : other.files.iter().chain(self.files.iter()).map(|(k, v)| (k.clone(), v.clone())).collect(),
+            subfolders : other.subfolders.iter().chain(self.subfolders.iter()).map(|(k, v)| (k.clone(), v.clone())).collect(),
         }
     }
 
 
     fn add_subfolder(&mut self, name: String, folder: Folder) {
-        self.subfolders.insert(name, folder);
-        // self.subfolders.entry(name).and_modify(|dir| dir.merge(folder))
-        // TODO: merge shit
+        match self.subfolders.remove(&name) {
+            None => self.subfolders.insert(name, folder),
+            Some(f) => self.subfolders.insert(name, f.merge(folder))
+        };
+
     }
 
     fn add_file(&mut self, name: String, file: File) {
         self.files.insert(name, file);
     }
-    
-    // fn add_files(&mut self, files: &mut dyn Iterator<Item = File>) {
-    //     self.files.extend(files);
-    // }
-
 }
 
 #[derive(Debug, Clone)]
@@ -86,10 +65,7 @@ struct File {
 impl File {
     fn from_string(line: &str) -> File {
         println!("{line}");
-        File {
-            size : line.trim().split_whitespace().next().unwrap().parse().expect("ls output is fixed"),
-            name : line.trim().split_whitespace().nth(1).unwrap().to_string(),
-        }
+        File { size : line.trim().split_whitespace().next().unwrap().parse().expect("ls output is fixed") }
         
     }
 }
@@ -102,30 +78,31 @@ fn main() {
 }
 
 fn parse(text: &String) {
-    let root = &mut Folder::new_empty();
+    let mut root = Folder::new_empty();
     let mut cwd = root;
-    let _ = text.split("$")
-            .skip(1)
-            .map(|block| {
-                println!("Parsing block:\n{block}");
-                let mut lines = block.lines();
-                match lines.next() {
-                    None => (),
-                    Some(line) => {
-                        let mut words = line.split_whitespace();
-                        match words.next().unwrap() {
-                            // "cd" => {
-                            //     let name = words.next().unwrap().to_string();
-                            //     cwd.add_subfolder(&name, Folder::new_empty());
-                            //     cwd = cwd.subfolders.get_mut(&name).unwrap();
-                            // },
-                            // // "ls" => {let files: Vec<Files> = lines.map(|line| File::from_string(line)).collect();},
-                            // "ls" => root.add_files(&mut lines.map(|line| File::from_string(line)).into_iter()),
-                            _ => panic!(),
-                        }
+
+    text.split("$")
+        .skip(1)
+        .map(|block| {
+            println!("Parsing block:\n{block}");
+            let mut lines = block.lines();
+            match lines.next() {
+                None => (),
+                Some(line) => {
+                    let mut words = line.split_whitespace();
+                    match words.next().unwrap() {
+                        // "cd" => {
+                        //     let name = words.next().unwrap().to_string();
+                        //     cwd.add_subfolder(&name, Folder::new_empty());
+                        //     cwd = cwd.subfolders.get_mut(&name).unwrap();
+                        // },
+                        // // "ls" => {let files: Vec<Files> = lines.map(|line| File::from_string(line)).collect();},
+                        // "ls" => root.add_files(&mut lines.map(|line| File::from_string(line)).into_iter()),
+                        _ => panic!(),
                     }
                 }
-            })
-            .collect::<Vec<_>>();
+            }
+        })
+        .collect::<Vec<_>>();
     // dbg!(root.size());
 }
