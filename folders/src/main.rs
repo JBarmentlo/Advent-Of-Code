277121ef72a86a805e3c98099c8605f4e68e94d8
Joep B <joepbarmentlo@gmail.com>
2024-02-25 19:22:16 +0100

    moving

--- folders/src/main.rs
+++ folders/src/main.rs
@@ -1,21 +1,11 @@
 use std::collections::HashMap;
 use std::collections::hash_map::Iter;
 use std::fs;
-use std::io::Cursor;
-use std::iter::Filter;
-
-fn main() { 
-    println!("Hello, world!");
-    let contents = fs::read_to_string("data.txt").expect("The file is static and is always parsable");
-    let root = parse(&contents);
-    let sum = sum_larger(&root, 100000);
-    dbg!(sum);
-}
 
 #[derive(Debug)]
-enum Fuck { 
+enum FsObject { 
     File(u32),
-    Folder(HashMap<String, Fuck>)
+    Folder(HashMap<String, FsObject>)
 }
 
 
@@ -25,15 +15,26 @@ struct SizeCounter {
     counted: u32
 }
 
-fn sum_larger(root: &Fuck, max_limit: u32) -> SizeCounter {
+fn main() { 
+    println!("Hello, world!");
+    let contents = fs::read_to_string("data.txt").expect("The file is static and is always parsable");
+    let root = parse(&contents);
+    let sum = sum_larger(&root, 100000);
+    dbg!(sum);
+}
+
+
+
+
+fn sum_larger(root: &FsObject, max_limit: u32) -> SizeCounter {
     match root {
-        Fuck::File(size) => {
+        FsObject::File(size) => {
             SizeCounter{
                 total: *size, 
                 counted: 0,
             }
         },
-        Fuck::Folder(map) => {
+        FsObject::Folder(map) => {
             let mut out = map.values()
             .map(|f| sum_larger(f, max_limit))
             .fold(
@@ -47,22 +48,23 @@ fn sum_larger(root: &Fuck, max_limit: u32) -> SizeCounter {
             if out.total < max_limit {
                 out.counted += out.total;
             }
+
             out
         }
     }
 }
 
-fn get_mut_recursive<'a>(mut names: impl Iterator<Item=&'a String>, fuck: &mut Fuck) -> &mut HashMap<String, Fuck> {
+fn get_mut_recursive<'a>(mut names: impl Iterator<Item=&'a String>, fuck: &mut FsObject) -> &mut HashMap<String, FsObject> {
     match names.next() {
         None => {
-            if let Fuck::Folder(ref mut map) = fuck {
+            if let FsObject::Folder(ref mut map) = fuck {
                 map
             } else {
                 panic!("fuck")
             }
         },
         Some(name) => {
-            if let Fuck::Folder(map) = fuck {
+            if let FsObject::Folder(map) = fuck {
                 get_mut_recursive(names, map.get_mut(name).expect("Fuck"))
             } else {
                 panic!("fuck")
@@ -71,10 +73,10 @@ fn get_mut_recursive<'a>(mut names: impl Iterator<Item=&'a String>, fuck: &mut F
     }
 }
 
-fn parse(text: &String) -> Fuck {
-    let mut root_fuck = Fuck::Folder(HashMap::new());
+fn parse(text: &String) -> FsObject {
+    let mut root_fuck = FsObject::Folder(HashMap::new());
     let mut current_fucks: Vec<String> = Vec::new();
-    get_mut_recursive(current_fucks.iter(), &mut root_fuck).insert("root".to_string(), Fuck::Folder(HashMap::new()));
+    get_mut_recursive(current_fucks.iter(), &mut root_fuck).insert("root".to_string(), FsObject::Folder(HashMap::new()));
     current_fucks.push("root".to_string());
 
 
@@ -98,7 +100,7 @@ fn parse(text: &String) -> Fuck {
                     current_fucks = vec!["root".to_string()];
                 } else {
                     let map = get_mut_recursive(current_fucks.iter(), &mut root_fuck);
-                    map.entry(arg.to_string()).or_insert(Fuck::Folder(HashMap::new()));
+                    map.entry(arg.to_string()).or_insert(FsObject::Folder(HashMap::new()));
                     current_fucks.push(arg.to_string());
                 }
             },
@@ -112,7 +114,7 @@ fn parse(text: &String) -> Fuck {
                         let file_size: u32 = words.next().unwrap().parse().expect("Std format expected");
                         let file_name = words.next().unwrap();
                         let map = get_mut_recursive(current_fucks.iter(), &mut root_fuck);
-                        map.insert(file_name.to_string(), Fuck::File(file_size));
+                        map.insert(file_name.to_string(), FsObject::File(file_size));
                     }
                 }
             },
