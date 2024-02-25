13b82c398ec7af84a0a9a62db62c97df8c09d793
Joep B <joepbarmentlo@gmail.com>
2024-02-25 14:02:26 +0100

    WIP: printing

--- folders/src/main.rs
+++ folders/src/main.rs
@@ -1,101 +1,5 @@
-use std::{borrow::BorrowMut, collections::HashMap, fs, rc::{Rc, Weak}};
-
-pub trait Sizeable {
-    fn size(&self) -> u32;
-}
-
-#[derive(Debug, Clone, Default)]
-struct Folder {
-    files: HashMap<String, File>,
-    subfolders: HashMap<String, Folder>,
-    parent: Weak<Folder>
-}
-
-// #[derive(Debug)]
-// struct Node {
-//     folder: Folder,
-//     parent: Weak<Node>,
-//     children: HashMap<String, Node>,
-// }
-
-// impl Node {
-//     fn empty() -> Node {
-//         Node {
-//             folder : Folder::new_empty(),
-//             children: HashMap::new()
-//         }
-//     }
-
-//     fn add_child(&mut self, name: &String, child: Node) {
-//         self.children.insert(name.clone(), child);
-//     }
-// }
-
-impl Folder {
-    fn new(files: Option<HashMap<String, File>>, subfolders: Option<HashMap<String, Folder>>, parent: Option<Folder>) -> Folder {
-        let files = files.unwrap_or_default();
-        let subfolders = subfolders.unwrap_or_default();
-        let parent = parent.unwrap_or_default();
-        let parent = Rc::downgrade(&Rc::new(parent));
-        // dbg!(&files);
-        Folder {
-            files,
-            subfolders,
-            parent,
-        }
-    }
-
-    fn new_empty() -> Folder {
-        let files: HashMap<String, File> = HashMap::new();
-        let subfolders: HashMap<String, Folder>= HashMap::new();
-        let parent = Weak::new();
-        Folder {
-            files,
-            subfolders,
-            parent,
-        }
-    }
-
-    fn merge(self, other: Folder) -> Folder {
-        Folder {
-            files : other.files.iter().chain(self.files.iter()).map(|(k, v)| (k.clone(), v.clone())).collect(),
-            subfolders : other.subfolders.iter().chain(self.subfolders.iter()).map(|(k, v)| (k.clone(), v.clone())).collect(),
-        }
-    }
-
-
-    fn add_subfolder(&mut self, name: String, folder: Folder) {
-        match self.subfolders.remove(&name) {
-            None => self.subfolders.insert(name, folder),
-            Some(f) => self.subfolders.insert(name, f.merge(folder))
-        };
-    }
-
-    fn add_file(&mut self, name: String, file: File) {
-        self.files.insert(name, file);
-    }
-
-    fn add_text_line(&mut self, line: &str) {
-        if line.trim().starts_with("dir") {
-            self.add_subfolder(line.trim().split_whitespace().nth(1).expect("should be there").to_string(), Folder::new_empty())
-        } else {
-            let mut words = line.trim().split_whitespace();
-            let size: u32 = words.next().expect("should be there").parse().expect("should be there");
-            let name = words.next().expect("should be there");
-            self.add_file(name.to_string(), File {size});
-        }
-    }
-
-    fn size(&self) -> u32 {
-        self.files.values().fold(0, |a, b| a + b.size) + self.subfolders.values().fold(0,|a, b| a + b.size())
-    }
-}
-
-#[derive(Debug, Clone)]
-struct File {
-    size: u32,
-}
-
+use std::collections::HashMap;
+use std::fs;
 
 fn main() { 
     println!("Hello, world!");
@@ -105,38 +9,70 @@ fn main() {
 }
 
 fn parse(text: &String) {
-    let mut root = Folder::new_empty();
-    let mut cwd = &mut root;
-
-    let blocks = text.split("$")
-        .skip(1);
+    // let mut root = Folder::new_empty();
+    // let mut cwd = &mut root;
+    let mut folder_names: Vec<String> = Vec::new();
+    let mut files: HashMap<String, u32> = HashMap::new();
+    let blocks = text.split("$ ")
+        .skip(2);
 
     for block in blocks {
+        println!("{block}");
         let mut lines = block.lines();
         let command_line = lines.next().expect("every block has a first line").trim();
         let mut command_line_words = command_line.split_whitespace();
         let cmd = command_line_words.next().expect("always here");
         let arg = command_line_words.next();
-        
         let respones_lines = lines;
 
+        println!("{command_line}");
         match cmd {
             "cd" => {
-                let arg = arg.expect("always arg for cd"); 
-                cwd.add_subfolder(arg.to_string(), Folder::new_empty());
-                cwd = cwd.subfolders.get_mut(arg).expect("Just added it");
+                let arg = arg.expect("always arg for cd");
+                if arg == ".." {
+                    folder_names.pop();
+                } else {
+                    folder_names.push(arg.to_string());
+                }
+                let cwd = folder_names.join("/");
+                println!("\tcurrent_folder: {cwd}");
             },
             
             "ls" => {
                 for line in respones_lines {
-                    cwd.add_text_line(line);
+                    println!("\t{line}");
+                    let trimmed_line = line.trim();
+                    if  !trimmed_line.starts_with("dir") {
+                        let mut words = trimmed_line.split_whitespace();
+                        let file_size: u32 = words.next().unwrap().parse().expect("Std format expected");
+                        let file_name = words.next().unwrap();
+                        let full_path = folder_names.join("/") + "/" + file_name;
+                        // println!("\t\tAdd: {full_path}: {file_size}");
+                        files.insert(full_path, file_size);
+                    }
                 }
             },
 
             _ => ()
         }
+        println!();
+        println!();
     }
-    let size = root.size();
-    dbg!(root);
-    dbg!(size);
+    dbg!(&files);
+    let total_size: u32 = files.values().sum();
+    dbg!(total_size);
+    parse_file_map(files);
+}
+
+enum FsContent {
+    File(u32),
+    Folder(HashMap<String, FsContent>)
+}
+
+fn parse_file_map(files: HashMap<String, u32>){
+    // Folder(files.iter().filter)
+    for (n, s) in files.iter() {
+        let count = n.split("/").count() - 2;
+        println!("{count} : {n}");
+    }
 }
