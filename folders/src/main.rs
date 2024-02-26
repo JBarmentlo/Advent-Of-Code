6b6a8ede2cc13d26a7f7bbbf660220b2ecc3fcb9
Joep B <joepbarmentlo@gmail.com>
2024-02-26 12:29:37 +0100

    WIP

--- folders/src/main.rs
+++ folders/src/main.rs
@@ -8,6 +8,29 @@ enum OldFsObject {
 }
 
 
+#[derive(Debug)]
+enum FsObject { 
+    File(File),
+    Folder(Folder)
+}
+
+impl FsObject {
+    fn new_folder(name: String) -> FsObject {
+        FsObject::Folder(Folder::from_name(name))
+    }
+
+    fn new_file(name: String, size: u32) -> FsObject {
+        FsObject::File(File{name, size})
+    }
+
+    fn add_folder(&mut self, folder_to_add: Folder) {
+        match self {
+            FsObject::Folder(ref mut folder) => folder.contents.insert(folder_to_add.name.clone(), FsObject::Folder(folder_to_add)),
+            _ => panic!("trying to insert into file")
+        };
+    }
+}
+
 #[derive(Debug, Default)]
 struct Folder {
     name: String,
@@ -16,31 +39,41 @@ struct Folder {
 }
 
 impl Folder {
-    fn get_nested_folder(&mut self, mut names: impl Iterator<Item=&'a String>) -> &mut Folder {
+    fn get_mut_nested_folder<'a>(&mut self, mut names: impl Iterator<Item=&'a String>) -> &mut Folder {
         match names.next() {
             None => self,
             Some(name) => {
                 match self.contents.get_mut(name).expect("Looking for missing folder") {
                     FsObject::Folder(ref mut folder) => {
-                        folder.get_nested_folder(names)
+                        folder.get_mut_nested_folder(names)
                     }
                     _ => panic!("cd into a file"),
                 }
             }
         }
-    }    
+    }
+
+    fn from_name(name: String) -> Folder {
+        return Folder {
+            name, 
+            size: None,
+            contents: HashMap::new(),
+        }
+    }
+
+    fn add_folder(&mut self, folder_to_add: Folder) {
+        self.contents.insert(folder_to_add.name.clone(), FsObject::Folder(folder_to_add));
+    }
+
+    fn add_file(&mut self, file_to_add: File) {
+        self.contents.insert(file_to_add.name.clone(), FsObject::File(file_to_add));
+    }
 }
 
 #[derive(Debug, Default)]
 struct File {
     name: String,
-    size: Option<u32>,
-}
-
-#[derive(Debug, Default)]
-enum FsObject { 
-    File(File),
-    Folder(Folder)
+    size: u32,
 }
 
 #[derive(Debug, Default)]
@@ -52,89 +85,50 @@ struct SizeCounter {
 fn main() { 
     println!("Hello, world!");
     let contents = fs::read_to_string("data.txt").expect("The file is static and is always parsable");
+    // let root = parse(&contents);
     let root = parse(&contents);
-    parse_new(&contents);
     let sum = sum_folders_smaller_than(&root, 100000);
     println!("The first answer is {0}", sum.counted);
 
-    let total_space: i32 = 70000000;
-    let required_space: i32  = 30000000;
-    let missing_space: i32 = required_space - total_space + sum.total as i32;
-    println!("Missing space: {missing_space}");
-    // if missing_space < 0 {
-    //     panic!("nope");
-    // } else {
-    //     find_smallest_folder_bigger_than("root".to_string(), &root, missing_space as u32);
-    // }
+    // let total_space: i32 = 70000000;
+    // let required_space: i32  = 30000000;
+    // let missing_space: i32 = required_space - total_space + sum.total as i32;
+    // println!("Missing space: {missing_space}");
+
 }
 
 
-fn sum_folders_smaller_than(root: &OldFsObject, max_limit: u32) -> SizeCounter {
-    match root {
-        OldFsObject::File(size) => {
-            SizeCounter{
-                total: *size, 
-                counted: 0,
-            }
+fn sum_folders_smaller_than(root: &FsObject, max_limit: u32) -> (FsObject, u32) {
+    let counted = match root {
+        FsObject::File(file) => {
+            0
         },
-        OldFsObject::Folder(map) => {
-            let mut out = map.values()
+        FsObject::Folder(folder) => {
+            folder.contents.values()
             .map(|f| sum_folders_smaller_than(f, max_limit))
             .fold(
-                SizeCounter::default(), 
-                |a, b| SizeCounter{
-                    total: a.total + b.total,
-                    counted: a.counted + b.counted,
+                (FsObject::new_file(0, 0), 0),
+                |(fs_a, total_a), (fs_b, total_b)| {
+                    let mut total = total_a + total_b;
+                    match fs_a {
+                        FsObject::Folder(folder) => {
+                            total += folder.size;
+                            
+                        }
+                        
+
+                    }
                 }
-            );
             
-            if out.total < max_limit {
-                out.counted += out.total;
-            }
-
-            out
         }
     }
 }
 
-fn get_mut_recursive<'a>(mut names: impl Iterator<Item=&'a String>, fuck: &mut OldFsObject) -> &mut HashMap<String, OldFsObject> {
-    match names.next() {
-        None => {
-            if let OldFsObject::Folder(ref mut map) = fuck {
-                map
-            } else {
-                panic!("fuck")
-            }
-        },
-        Some(name) => {
-            if let OldFsObject::Folder(map) = fuck {
-                get_mut_recursive(names, map.get_mut(name).expect("Fuck"))
-            } else {
-                panic!("fuck")
-            }
-        }
-    }
-}
-
-
-
-
-fn parse_new(text: &String) -> Folder {
-    let mut root = Folder::default();
+fn parse(text: &String) -> Folder {
+    let mut root = Folder::from_name("root".to_string());
     let mut current_dirs: Vec<String> = Vec::new();
-
-}
-
-
-fn parse(text: &String) -> OldFsObject {
-    let mut root_fuck = OldFsObject::Folder(HashMap::new());
-    let mut current_fucks: Vec<String> = Vec::new();
-    get_mut_recursive(current_fucks.iter(), &mut root_fuck).insert("root".to_string(), OldFsObject::Folder(HashMap::new()));
-    current_fucks.push("root".to_string());
-
-
-    let blocks = text.split("$ ")
-        .skip(2);
+    
+    let blocks = text.split("$ ").skip(2);
 
     for block in blocks {
         let mut lines = block.lines();
@@ -143,18 +137,20 @@ fn parse(text: &String) -> OldFsObject {
         let cmd = command_line_words.next().expect("always here");
         let arg = command_line_words.next();
         let respones_lines = lines;
+        let cwd = root.get_mut_nested_folder(current_dirs.iter());
 
         match cmd {
             "cd" => {
                 let arg = arg.expect("always arg for cd");
                 if arg == ".." {
-                    current_fucks.pop();
+                    current_dirs.pop();
                 } else if arg == "/" {
-                    current_fucks = vec!["root".to_string()];
+                    current_dirs = Vec::new();
                 } else {
-                    let map = get_mut_recursive(current_fucks.iter(), &mut root_fuck);
-                    map.entry(arg.to_string()).or_insert(OldFsObject::Folder(HashMap::new()));
-                    current_fucks.push(arg.to_string());
+                    if !cwd.contents.contains_key(arg) {
+                        cwd.add_folder(Folder::from_name(arg.to_string()));
+                    }
+                    current_dirs.push(arg.to_string());
                 }
             },
             
@@ -164,15 +160,14 @@ fn parse(text: &String) -> OldFsObject {
                     
                     if !trimmed_line.starts_with("dir") {
                         let mut words = trimmed_line.split_whitespace();
-                        let file_size: u32 = words.next().unwrap().parse().expect("Std format expected");
-                        let file_name = words.next().unwrap();
-                        let map = get_mut_recursive(current_fucks.iter(), &mut root_fuck);
-                        map.insert(file_name.to_string(), OldFsObject::File(file_size));
+                        let size: u32 = words.next().unwrap().parse().expect("Std format expected");
+                        let name = words.next().unwrap().to_string();
+                        cwd.add_file(File{name, size})
                     }
                 }
             },
             _ => ()
         }
     }
-    root_fuck
+    root
 }
